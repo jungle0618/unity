41d1ffccf64580d619d4ed6c97fbe728b58cbcd3
LexicalError <reyuna000@gmail.com>
2025-12-02 07:45:17 +0800

    Tweak player animations, add enemy animation controller

---
+++ Assets/script/Entity/Enemy/EnemyAnimationController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+    [SerializeField] private Enemy enemy;
+    [SerializeField] private Animator animator;
+    private int currentWeapon = -1;
+    private float speed = 0f;
+
+    public void TriggerAttackAnimation3D()
+    {   
+        if (currentWeapon == -1) return;
+        animator.SetTrigger("Shoot");
+    }
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        speed = enemy.GetSpeed();
+        
+        animator.SetFloat("Speed", speed);
+        
+        animator.SetInt("weaponState", 1);
+    }
+}
